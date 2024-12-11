250bc6b849ee8ede5863def1d64eaec2babd4ef6
Pratik <pgumthanavar05@gmail.com>
2024-12-11 19:41:50 +0530

    Notes and scripts on functions.

---
+++ functions.js
@@ -0,0 +1,40 @@
+// Function Statement // Function Declaration
+function a(){
+    var ca = 10
+    console.log(ca)
+}
+a();
+
+
+// Function expression - function is passed as a value of a variable
+var b = function (){
+    console.log("Hello")
+}
+b();
+
+
+// Anonymous Function - function without name
+// function () {
+
+// }
+
+
+// Named Function Expression
+var ab = function m(){
+    console.log("Hello2")
+}
+ab();
+// m();    ReferenceError: m is not defined
+
+
+// Difference Between Parameters and Arguments
+function l(q, w){     // q, w are the parameters of the function
+    console.log(q, w);
+}
+l(2, 0);    // 2, 0 are the arguments passed to the function
+
+// First Class Functions  - first class citizens
+// ability to be used as values or arguemnts
+
+
+// Arrow Functions
